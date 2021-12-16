fa481be869ba86c72523ad596e17e0665f8449f6
addmix <38119387+addmix@users.noreply.github.com>
2021-12-16 02:11:44 -0800

    Add files via upload

---
+++ classes/v2spring.gd
@@ -0,0 +1,56 @@
+extends Resource
+
+class_name V2Spring
+
+var position := Vector2.ZERO
+var velocity := Vector2.ZERO
+var target := Vector2.ZERO
+var damper : float = 0
+var speed : float = 1
+var mass : float = 1
+
+#base funcs
+func _init(p := Vector2(0, 0), v := Vector2(0, 0), t := Vector2(0, 0), d := 0.5, s := 1.0) -> void:
+	position = p
+	velocity = v
+	target = t
+	damper = d
+	speed = s
+
+func get_class() -> String:
+	return "V2Spring"
+
+
+#this basically just runs 2 1d springs together
+#im pretty sure this will be effected by gimbal lock
+#physics funcs
+func positionvelocity(delta : float) -> void:
+	if damper >= 1:
+		return
+	if speed == 0:
+		push_error("Speed == 0 on V2Spring")
+	
+	var direction = position - target
+	
+	#round curve
+	var curve = pow(1 - damper * damper, .5)
+	
+	#weird exponetial thingy
+	var curve1 : Vector2 = (velocity / speed + damper * direction) / curve
+	
+	#hanging rope
+	var cosine : float = cos(curve * speed * delta)
+	
+	#deflated bubble
+	var sine : float = sin(curve * speed * delta)
+	
+	var e : float = pow(2.718281828459045, damper * speed * delta)
+	
+	position = target + (direction * cosine + curve1 * sine) / e
+	velocity = speed * ((curve * curve1 - damper * direction) * cosine - (curve * direction + damper * curve1) * sine) / e
+
+func apply_force(f : Vector2) -> void:
+	velocity += f / mass
+
+func accelerate(s : Vector2) -> void:
+	velocity += s
