8d9cfb74bcf332022181bad013d6c4e1e2650bc6
addmix <38119387+addmix@users.noreply.github.com>
2021-12-16 02:11:13 -0800

    Update v3spring.gd

--- classes/v3spring.gd
+++ classes/v3spring.gd
@@ -55,18 +55,3 @@ func apply_force(f : Vector3) -> void:
 
 func accelerate(s : Vector3) -> void:
 	velocity += s
-
-
-#networking
-
-
-func get_networking() -> Array:
-	return [position, velocity, target]
-
-func set_networking(a : Array) -> void:
-	position = a[0]
-	velocity = a[1]
-	target = a[2]
-
-func check_discrepancy(s : V3Spring, d : float) -> bool:
-	return (position - s.position).length() < d
