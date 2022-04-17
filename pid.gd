0ef69f4204178d4c982ee29371ab88fab2ab0bc8
addmix <addmixtaylor@gmail.com>
2022-04-17 15:29:50 -0600

    Create pid.gd

---
+++ pid.gd
@@ -0,0 +1,24 @@
+extends Reference
+class_name PID
+
+var p : float = 0.2
+var i : float = 0.05
+var d : float = 1.0
+
+var value : float = 0
+
+var _last_error : float
+var _integral : float
+
+func _init(p : float, i : float, d : float) -> void:
+	self.p = p
+	self.i = i
+	self.d = d
+
+func update(delta : float, error : float) -> float:
+	var derivative : float = (error - _last_error) / delta
+	_integral += error * delta
+	_last_error = error
+	
+	value = clamp(p * error + i * _integral + d * derivative, 0.0, 1.0)
+	return value
