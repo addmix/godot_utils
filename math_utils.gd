8e50494b7d818a978ac23cb34c79ec3baa8afaab
addmix <addmixtaylor@gmail.com>
2022-06-29 16:05:57 -0600

    added closest_point_on_line functions

--- math_utils.gd
+++ math_utils.gd
@@ -70,6 +70,15 @@ static func v3_rand_range(v1 : Vector3, v2 : Vector3) -> Vector3:
 
 	return Vector3(x, y, z)
 
+static func closest_point_on_line_clamped(a : Vector3, b : Vector3, c : Vector3) -> Vector3:
+	b = (b - a).normalized()
+	c = c - a
+	return a + b * clamp(c.dot(b), 0.0, 1.0)
+
+static func closest_point_on_line(a : Vector3, b : Vector3, c : Vector3) -> Vector3:
+	b = (b - a).normalized()
+	c = c - a
+	return a + b * (c.dot(b))
 
 
 #transform/basis math
