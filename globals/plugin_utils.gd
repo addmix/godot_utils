6f92529923edd5c6c2a219dccde8c8e7c2eba179
addmix <addmixtaylor@gmail.com>
2022-10-21 21:00:14 -0600

    Removed ifndef from plugin utils
    New location is under SettingsUtils

--- globals/plugin_utils.gd
+++ globals/plugin_utils.gd
@@ -1,8 +1,1 @@
 class_name PluginUtils
-
-static func ifndef(setting : String, default_value) -> Variant:
-	if not ProjectSettings.has_setting(setting):
-		ProjectSettings.set_setting(setting, default_value)
-		ProjectSettings.set_initial_value(setting, default_value)
-	
-	return ProjectSettings.get_setting(setting)
