7f2797edaf87ac32e765ef2c64a210466e7922b9
addmix <addmixtaylor@gmail.com>
2022-10-21 20:59:17 -0600

    Create settings_utils.gd

---
+++ globals/settings_utils.gd
@@ -0,0 +1,8 @@
+class_name SettingsUtils
+
+static func ifndef(setting : String, default_value) -> Variant:
+	if not ProjectSettings.has_setting(setting):
+		ProjectSettings.set_setting(setting, default_value)
+		ProjectSettings.set_initial_value(setting, default_value)
+	
+	return ProjectSettings.get_setting(setting)
