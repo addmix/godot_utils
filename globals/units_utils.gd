b3cf90af9dd165707b2c332c9757710e60d8a9eb
addmix <addmixtaylor@gmail.com>
2023-02-07 00:55:02 -0700

    Added acceleration unit conversions.

--- globals/units_utils.gd
+++ globals/units_utils.gd
@@ -21,3 +21,9 @@ static func meters_to_kilometers(m : float) -> float:
 
 static func meters_to_miles(m : float) -> float:
 	return m * 0.0006213712
+
+static func meters_per_second_squared_to_gs(ms2 : float) -> float:
+	return ms2 * 0.1019716213
+
+static func meters_per_second_squared_to_feet_per_second_squared(ms2 : float) -> float:
+	return meters_to_feet(ms2)
