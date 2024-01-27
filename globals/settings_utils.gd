4e8e5596e3787075ff3abc3adbc852b37d0b6244
addmix <38119387+addmix@users.noreply.github.com>
2024-01-26 21:33:36 -0700

    Ensure static typing for ifndef

--- globals/settings_utils.gd
+++ globals/settings_utils.gd
@@ -1,6 +1,6 @@
 class_name SettingsUtils
 
-static func ifndef(setting : String, default_value) -> Variant:
+static func ifndef(setting : String, default_value : Variant) -> Variant:
 	if not ProjectSettings.has_setting(setting):
 		ProjectSettings.set_setting(setting, default_value)
 		ProjectSettings.set_initial_value(setting, default_value)
