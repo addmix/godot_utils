165b9d333b0dadbf195fe52b01e5260878c9d73a
addmix <38119387+addmix@users.noreply.github.com>
2024-02-18 04:23:39 -0700

    Add `get_altitude()` function to `FloatingOriginHelper`

--- singletons/floating_origin_helper.gd
+++ singletons/floating_origin_helper.gd
@@ -3,7 +3,7 @@ extends Node
 
 var floating_origin : FloatingOrigin
 
-func get_global_position(node : Node) -> Vector3:
+func get_global_position(node : Node3D) -> Vector3:
 	if floating_origin:
 		return node.global_position + floating_origin.global_offset
 	else:
@@ -14,3 +14,6 @@ func adjust_global_position(global_position : Vector3) -> Vector3:
 		return global_position + floating_origin.global_offset
 	else:
 		return global_position
+
+func get_altitude(node : Node3D) -> float:
+	return get_global_position(node).y
