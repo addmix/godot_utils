2c32c08c830785288346a47e8fc590947a0cf186
addmix <38119387+addmix@users.noreply.github.com>
2024-02-18 03:10:35 -0700

    Make FloatingOriginHelper safe.

--- singletons/floating_origin_helper.gd
+++ singletons/floating_origin_helper.gd
@@ -3,5 +3,14 @@ extends Node
 
 var floating_origin : FloatingOrigin
 
-func get_global_position(node : Node) -> Vector3: return node.global_position + floating_origin.global_offset
-func adjust_global_position(global_position : Vector3) -> Vector3: return global_position + floating_origin.global_offset
+func get_global_position(node : Node) -> Vector3:
+	if floating_origin:
+		return node.global_position + floating_origin.global_offset
+	else:
+		return node.global_position
+
+func adjust_global_position(global_position : Vector3) -> Vector3: 
+	if floating_origin:
+		return global_position + floating_origin.global_offset
+	else:
+		return global_position
