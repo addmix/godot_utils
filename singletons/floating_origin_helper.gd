a404a47a8b09c6ab305426e62276a86b854b789c
addmix <38119387+addmix@users.noreply.github.com>
2024-05-04 16:52:53 -0600

    Added `FloatingOriginHelper.get_global_transform(Node3D)`

--- singletons/floating_origin_helper.gd
+++ singletons/floating_origin_helper.gd
@@ -9,6 +9,12 @@ func get_global_position(node : Node3D) -> Vector3:
 	else:
 		return node.global_position
 
+func get_global_transform(node : Node3D) -> Transform3D:
+	if floating_origin:
+		return node.global_transform * Transform3D(Basis(), floating_origin.global_offset)
+	else:
+		return node.global_transform
+
 func adjust_global_position(global_position : Vector3) -> Vector3: 
 	if floating_origin:
 		return global_position + floating_origin.global_offset
