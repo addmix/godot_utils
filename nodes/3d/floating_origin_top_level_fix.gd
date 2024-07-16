b2c9b599e7bf3f76148a4211c6e6f392ceac1559
addmix <38119387+addmix@users.noreply.github.com>
2024-07-16 03:32:52 -0600

    Avoid error in FloatingOriginTopLevelFix from singleton reference.

--- nodes/3d/floating_origin_top_level_fix.gd
+++ nodes/3d/floating_origin_top_level_fix.gd
@@ -1,7 +1,8 @@
 extends Node
 
 func _init():
-	FloatingOriginHelper.origin_shifted.connect(_on_origin_shifted)
+	if has_node("/root/FloatingOriginHelper"):
+		get_node("/root/FloatingOriginHelper").origin_shifted.connect(_on_origin_shifted)
 
 func _on_origin_shifted(shift : Vector3) -> void:
 	if get_parent() is Node3D and get_parent().top_level:
