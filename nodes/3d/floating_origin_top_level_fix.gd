b0da148959d9f655b4251360521a37f91b321112
addmix <38119387+addmix@users.noreply.github.com>
2024-11-30 19:03:45 -0700

    Prevent benign error from FloatingOriginTopLevelFix

--- nodes/3d/floating_origin_top_level_fix.gd
+++ nodes/3d/floating_origin_top_level_fix.gd
@@ -2,7 +2,7 @@ extends Node
 
 func _enter_tree():
 	if has_node("/root/FloatingOriginHelper"):
-		get_node("/root/FloatingOriginHelper").origin_shifted.connect(_on_origin_shifted)
+		NodeUtils.connect_signal_safe(get_node("/root/FloatingOriginHelper"), "origin_shifted", _on_origin_shifted, 0, true)
 
 func _on_origin_shifted(shift : Vector3) -> void:
 	if get_parent() is Node3D and get_parent().top_level:
