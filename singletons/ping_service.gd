91eacc0fa8c6b20349f55e1845bf1ee920963c9b
addmix <38119387+addmix@users.noreply.github.com>
2025-01-01 14:55:19 -0700

    Add `PingService.rpc_reliable_timing()`

--- singletons/ping_service.gd
+++ singletons/ping_service.gd
@@ -15,6 +15,8 @@ func _physics_process(delta : float) -> void:
 		return
 	#ping everyone
 	_ping.rpc(Time.get_ticks_msec())
+	
+	#this should be sent less often.
 	receive_ping_dictionary.rpc(ping_dictionary)
 
 #called on host and client
@@ -46,8 +48,40 @@ func receive_ping_dictionary(ping_dictionary : Dictionary) -> void:
 		return
 	ping_dictionary = ping_dictionary
 
+#we should probably clarify naming that this is total ping between ourselves and the target peer ID
 func get_ping(id : int) -> float:
-	return ping_dictionary[id]
+	var total_ping : float = ping + ping_dictionary.get(id, -0.0)
+	
+	if id == multiplayer.get_unique_id():
+		ping = 0.0
+	
+	#use -0.0 as an indicator that no ping was available
+	#return ping_dictionary.get(id, -0.0)
+	return total_ping
 
 func on_peer_connected(id : int) -> void:
 	ping_dictionary[id] = 0.0
+
+#this function calls an RPC, trying to accommodate ping.
+#delay_time is how long, in seconds, to wait before the function should execute
+#if delay_time is lower than a peer's latency, they will be desynced.
+#function is the function to be called, which must be configured as an RPC.
+#params are any parameters for the function to be RPC'd
+func rpc_reliable_timing(delay_time : float, function : Callable, params : Array = []) -> void:
+	print("starting rpc")
+	for peer_id : int in MultiplayerUtils.get_connected_peer_ids(multiplayer):
+		var rpc_latency : float = ping + get_ping(peer_id)
+		#ping to self is 0
+		#this could be wrong if RPCs configured with call_local still propagate from client to server, back to client.
+		if peer_id == multiplayer.get_unique_id():
+			rpc_latency = 0.0
+		
+		var time_to_wait : float = delay_time - rpc_latency
+		
+		var delayed_rpc : Callable = func(time_to_delay : float, peer_id : int):
+			await get_tree().create_timer(time_to_wait).timeout
+			print("calling rpc for peer ", peer_id)
+			var function_with_arguments_bound := function.bindv(params)
+			function_with_arguments_bound.rpc_id(peer_id)
+		
+		delayed_rpc.call(time_to_wait, peer_id)
